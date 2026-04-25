7931098ce4b4ed084e90c7fe0e22bdf1b81a109c
doppalapudiyasaswini-glitch <doppalapudiyasaswini@gmail.com>
2026-04-25 10:15:21 +0530

    Add files via upload

---
+++ and_gate.v
@@ -0,0 +1,7 @@
+module and_g(a,b,y);
+  input a,b;
+  output reg y;
+  always@(*)begin
+    y=a&b;
+  end
+endmodule
---
+++ tb_nand_gate.v
@@ -0,0 +1,14 @@
+module tb;
+  reg a,b;
+  wire y;
+ nand_g dut(a,b,y);
+  integer i;
+  initial begin
+    for(i=0;i<4;i=i+1)begin
+   // {a,b}=$random;
+      {a,b}=i[1:0];
+    #1
+    $display("a=%0d b=%0d y=%0d",a,b,y);
+  end
+  end
+    endmodule*/
