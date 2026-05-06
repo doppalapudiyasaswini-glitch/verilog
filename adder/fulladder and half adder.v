bf62297fa211365d92ff663d621b4886dfa2f274
doppalapudiyasaswini-glitch <doppalapudiyasaswini@gmail.com>
2026-05-06 18:29:23 +0530

    Update and rename fulladder.v to fulladder and half adder.v

--- adder/fulladder and half adder.v
+++ adder/fulladder and half adder.v
@@ -1,17 +1,17 @@
-/*module full_adder(a,b,cin,sum,cout);
+module full_adder(a,b,cin,sum,cout);
 input a,b,cin;
 output reg sum,cout;
 always@(*)begin
   sum=a^b^cin;
   cout=(a&b)|(b&cin)|(a&cin);
 end
-endmodule*/
+endmodule
 
-module half_adder(a,b,sum,cout);
+/*module half_adder(a,b,sum,cout);
   input a,b;
   output reg sum,cout;
   always@(*)begin
     sum=a^b;
     cout=a&b;
   end
-endmodule
+endmodule*/
